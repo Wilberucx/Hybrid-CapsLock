588f55f3063b71cdc34bb79d7bcb2a538cd16a68
Wilberucx <wilber.u.canto29@gmail.com>
2025-09-08 07:15:06 -0500

    Feat: Mejora de Status and options tooltips

--- crear_acceso_directo.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Creando acceso directo en el escritorio...
-powershell "$WshShell = New-Object -comObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%USERPROFILE%\Desktop\HybridCapsLock v2.lnk'); $Shortcut.TargetPath = 'C:\Program Files\AutoHotkey\v2\AutoHotkey.exe'; $Shortcut.Arguments = '%CD%\HybridCapsLock_v2.ahk'; $Shortcut.WorkingDirectory = '%CD%'; $Shortcut.IconLocation = 'C:\Program Files\AutoHotkey\v2\AutoHotkey.exe'; $Shortcut.Save()"
-echo ¡Acceso directo creado en el escritorio!
-pause
